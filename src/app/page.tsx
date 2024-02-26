56e870aa5860294b192b0362761c2920810387f1
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-02-26 10:17:35 +0530

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Home() {
   return (
+    <main className="min-h-screen bg-black/[0.96] antialiased
+    bg-grid-white/[0.02">
     <h1 className="text-4xl text-center">Hello </h1>
+    </main>
   );
 }
