8a9e68a625c564a4e1bab49066ad8888b61f2983
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-02-26 17:05:45 +0530

    cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import HeroSection from "@/app/HeroSection";
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased
     bg-grid-white/[0.02">
     <HeroSection/>
+    <FeaturedCourses/>
     </main>
   );
 }
