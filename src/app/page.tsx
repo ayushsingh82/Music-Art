93e09e444d6b57b02f45c4009b6aac73ac9df113
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-02-26 11:49:07 +0530

    moving button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/app/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased
     bg-grid-white/[0.02">
-    <h1 className="text-4xl text-center">Hello </h1>
+    <HeroSection/>
     </main>
   );
 }
